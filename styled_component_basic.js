3cf3d1971bf022f8cc1a361a3cd0ef0de48b1c24
kjy5657 <45618920+kjy5657@users.noreply.github.com>
2022-01-20 21:20:21 +0900

    styled_component

--- styled_component_basic.js
+++ styled_component_basic.js
@@ -1,16 +1,11 @@
 import styled, { key, keyframes } from "styled-components";
 
-const Title = styled.h1`
-  color: ${(props) => props.theme.textColor};
-`;
-
 const Wrapper = styled.div`
   display: flex;
   height: 100vh;
   width: 100vw;
   justify-content: center;
   align-items: center;
-  background-color: ${(props) => props.theme.backgroundColor};
 `;
 
 const rotationAnimation = keyframes`
