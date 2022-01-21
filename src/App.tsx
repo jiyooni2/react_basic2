cb7b2f3c366912c12fe633990ab1d624ca1b6e23
kjy5657 <45618920+kjy5657@users.noreply.github.com>
2022-01-21 22:07:11 +0900

    TS concept(2)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,38 @@
-import Circle from "./Circle";
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+  color: ${(props) => props.theme.textColor};
+`;
 
 function App() {
+  const [value, setValue] = useState("");
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("hello", value);
+  };
+
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+
+    setValue(event.currentTarget.value);
+  };
   return (
     <div>
-      <Circle borderColor="yellow" bgColor="tomato" />
-      <Circle bgColor="teal" text="I'm here" />
+      <Container>Text</Container>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          value={value}
+          onChange={onChange}
+          placeholder="username"
+        ></input>
+        <button>LogIn</button>
+      </form>
     </div>
   );
 }
