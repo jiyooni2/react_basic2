1152e0379fbedf3d485f1bc091fbd41aad6ebf99
kjy5657 <45618920+kjy5657@users.noreply.github.com>
2022-01-21 21:30:32 +0900

    TS concept(1)

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Circle from "./Circle";
+
+function App() {
+  return (
+    <div>
+      <Circle borderColor="yellow" bgColor="tomato" />
+      <Circle bgColor="teal" text="I'm here" />
+    </div>
+  );
+}
+
+export default App;
